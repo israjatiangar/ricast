e0d26d6468a54e89881da9ec53b02d663f38ce0a
Rajat Iangar <10295339+israjatiangar@users.noreply.github.com>
2026-09-22 10:06:38 +0530

    add func to mod ui using api data
    ---
    - add loading screen and styling
    - style -> inline to external

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
 // @ts-check
 
-// #region TypeDefinitions
+// #region Type Definitions
 /** @typedef {Object} UserLocation
- * @property {string} cityName
- * @property {string} countryName
- * @property {number} longitude
- * @property {number} latitude
+ * @property {String} cityName
+ * @property {String} countryName
+ * @property {Number} longitude
+ * @property {Number} latitude
+ */
+
+/**
+ * @typedef {Object} WeatherData
+ * @property {Number} currentWeatherCode
+ * @property {Number} currentTemperature
+ * @property {Number} currentWindSpeed
+ * @property {Number} currentWindDirection
+ * @property {String} currentSunrise
+ * @property {String} currentSunset
+ * @property {Array <String>} weeklyDates
+ * @property {Array <String>} weeklyMinTemperature
+ * @property {Array <String>} weeklyMaxTemperature
+ * @property {Array <Number>} weeklyWeatherCode
  */
 // #endregion
 
+// #region Global Const and Class
 const CURRENT_DATE = new Date().toLocaleString('en', {
 	localeMatcher: 'best fit',
 	weekday: 'short',
@@ -17,15 +32,65 @@ const CURRENT_DATE = new Date().toLocaleString('en', {
 	year: 'numeric',
 })
 
-const Main = document.querySelector('main')
-const TodayCard = Main?.querySelector('#Today_Card')
-const TableForecast = Main?.querySelector('table')
+/** Delcared all App UI Element in a Class for easy access and better organisation
+ * @class appUIElements
+ */
+class appUIElements {
+	TodayCard =
+		/** @type {HTMLElement} */
+		(document.querySelector('#Today_Card'))
+	TableForecast =
+		/** @type {HTMLElement} */
+		(document.querySelector('#Weekly_Forecast'))
 
-const locationHeader = TodayCard?.querySelector('strong')
-const currentDateSubHead = TodayCard?.querySelector('small')
+	//Order of Appearance in DOM
+	//CurrentWeather Card
+	locationHeader =
+		/** @type {HTMLElement} */
+		(this.TodayCard.querySelector('strong'))
 
+	currentDateSubHead =
+		/** @type {HTMLElement} */
+		(this.TodayCard.querySelector('small'))
+
+	weatherIcon =
+		/** @type {HTMLElement} */
+		(this.TodayCard.querySelector('#Weather_Icon'))
+
+	temperatureMain =
+		/** @type {HTMLElement} */
+		(this.TodayCard.querySelector('#Temperature'))
+
+	windspeed =
+		/** @type {HTMLElement} */
+		(this.TodayCard.querySelector('#Windspeed'))
+	sunrise =
+		/** @type {HTMLElement} */
+		(this.TodayCard.querySelector('#Sunrise'))
+	sunset =
+		/** @type {HTMLElement} */
+		(this.TodayCard.querySelector('#Sunset'))
+}
+
+class LoadingElement {
+	loadingBackdrop = document.createElement('div')
+	loadingSpinner = document.createElement('span')
+	createLoader = () => {
+		this.loadingBackdrop.classList.add('loaderBackdrop')
+		this.loadingSpinner.classList.add('loader')
+		this.loadingBackdrop.append(this.loadingSpinner)
+		document.querySelector('body')?.append(this.loadingBackdrop)
+	}
+	removeLoader = () => {
+		this.loadingSpinner.remove()
+		this.loadingBackdrop.remove()
+	}
+}
+// #endregion
+
+// #region APIs that return Objects
 /** A function that fetches user's location data
- * @returns {Promise<UserLocation>}
+ * @returns {Promise<UserLocation>} Returns the UserLocation  Object
  * @throws Error if response fails
  */
 const getLocation = async () => {
@@ -34,6 +99,7 @@ const getLocation = async () => {
 		.catch(err => {
 			throw err.message
 		})
+	/**@type {UserLocation} */
 	const userLocation = {
 		cityName: fetchedLocation.city,
 		countryName: fetchedLocation.country,
@@ -44,12 +110,12 @@ const getLocation = async () => {
 }
 
 /** A function That returns weather Data longitude and latitude
- * @param {number} latitude The latitude of the location
- * @param {number} longitude The longitude of the location
- * @returns {Promise<Object>}
+ * @param {number} latitude The latitude of the location, default is 0
+ * @param {number} longitude The longitude of the location, default is 0
+ * @returns {Promise<WeatherData>}
  * @throws Error when Can't get the weather.
  */
-const getForecast = async (latitude, longitude) => {
+const getForecast = async (latitude = 0, longitude = 0) => {
 	const fetchedForecast = await fetch(
 		`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=sunrise,sunset,temperature_2m_min,temperature_2m_max,weather_code&current=temperature_2m,relative_humidity_2m,wind_speed_10m,wind_direction_10m&timezone=auto`
 	)
@@ -57,40 +123,144 @@ const getForecast = async (latitude, longitude) => {
 		.catch(err => {
 			throw err.message
 		})
-
-	return fetchedForecast
+	/** @type{WeatherData} */
+	const weatherData = {
+		currentWeatherCode: fetchedForecast.daily.weather_code[0],
+		currentTemperature: fetchedForecast.current.temperature_2m,
+		currentWindSpeed: fetchedForecast.current.wind_speed_10m,
+		currentWindDirection: fetchedForecast.current.wind_direction_10m,
+		currentSunrise: fetchedForecast.daily.sunrise[0],
+		currentSunset: fetchedForecast.daily.sunset[0],
+		weeklyDates: fetchedForecast.daily.time,
+		weeklyMinTemperature: fetchedForecast.daily.temperature_2m_min,
+		weeklyMaxTemperature: fetchedForecast.daily.temperature_2m_max,
+		weeklyWeatherCode: fetchedForecast.daily.weather_code,
+	}
+	return weatherData
 }
+// #endregion
+
+// #region Supplementary Functions
 /** A Function that takes in ridiculous weather code and returns emoji
- * @param {number} code - A WW weathercode
- * @returns {string} - Emoji corresponding to the code
+ * @param {Number} weatherCode  A WW weathercode
+ * @returns {String} Emoji Corresponding to the code
  */
-const decodeWeather = code => {
+const decodeWeather = weatherCode => {
 	const weatherEmoji = ['☀️', '⛅', '☁️', '🌧️', '❄️', '⛈️']
 
 	let emojiChoice =
-		code === 0 ? 0
-		: code <= 3 ? 1
-		: code <= 48 ? 2
-		: code <= 67 || (code >= 80 && code <= 82) ? 3
-		: code <= 86 ? 4
+		weatherCode === 0 ? 0
+		: weatherCode <= 3 ? 1
+		: weatherCode <= 48 ? 2
+		: weatherCode <= 67 || (weatherCode >= 80 && weatherCode <= 82) ? 3
+		: weatherCode <= 86 ? 4
 		: 5
 	return weatherEmoji[emojiChoice]
 }
-
-/** Takes a HTML Element clears it and inserts thingToInsert
- * @param {HTMLElement} htmlElement
- * @param {String} thingToInsert
+/** A function that takes in a WeatherCode and Returns a Hue String
+ * @param {Number} weatherCode A WW weathercode
+ * @returns {String} hue A 360 deg hue that matches the vibe of the weather
  */
-const clearElementAndInsert = (htmlElement, thingToInsert) => {
-	htmlElement.innerText = ''
-	if (thingToInsert !== null) {
-		htmlElement.innerText = thingToInsert
+const weatherToHue = weatherCode => {
+	let hue =
+		weatherCode === 0 ? '40'
+		: weatherCode <= 3 ? '90'
+		: weatherCode <= 48 ? '140'
+		: weatherCode <= 67 || (weatherCode >= 80 && weatherCode <= 82) ? '190'
+		: weatherCode <= 86 ? '240'
+		: '290'
+	return hue
+}
+/** A function that takes in number and returns Direction
+ * @param {Number} direction A Degree/Number from 0-360
+ * @returns {String} Caridinal Direction of Wind + Arrow
+ */
+const decodeWindDirection = direction => {
+	const windDirection =
+		direction < 23 ? '↑N'
+		: direction < 68 ? '↗NE'
+		: direction < 113 ? '→E'
+		: direction < 158 ? '↘SE'
+		: direction < 203 ? '↓S'
+		: direction < 248 ? '↙SW'
+		: direction < 293 ? '←W'
+		: '↑N'
+	return windDirection
+}
+/** A Function that takes in Date String And returns Date Or Time Based on the option Parameter
+ * @param {String} dateString
+ * @param {String} option Either time or dayName as String
+ * @returns string Value of Time or DayName Based on {option}
+ */
+const stringToDate = (dateString, option) => {
+	switch (option) {
+		case 'time':
+			const time = new Date(dateString).toLocaleTimeString('en-US', {
+				localeMatcher: 'best fit',
+				hour12: false,
+				timeStyle: 'short',
+			})
+			return time
+		case 'dayName':
+			const dayName = new Date(dateString).toLocaleDateString('en-US', {
+				weekday: 'long',
+			})
+			return dayName
 	}
 }
-const pageInit = () => {
-	alert('1')
+// #endregion
+
+// #region Initialising Funtion
+/**Updates the Whole Page with Information From the API*/
+const pageInit = async () => {
+	const Loader = new LoadingElement()
+	Loader.createLoader()
+	const weatherApp = new appUIElements()
+
+	const userLocation = await getLocation()
+	const weatherData = await getForecast(
+		userLocation.latitude,
+		userLocation.longitude
+	)
+	Loader.removeLoader()
+
+	weatherApp.TodayCard.setAttribute(
+		'data-ri-hue',
+		weatherToHue(weatherData.currentWeatherCode)
+	)
+	weatherApp.locationHeader.innerText = `${userLocation.cityName}, ${userLocation.countryName}`
+	weatherApp.currentDateSubHead.innerText = `${CURRENT_DATE}`
+
+	weatherApp.weatherIcon.innerText = `${decodeWeather(
+		weatherData.currentWeatherCode
+	)}`
+	weatherApp.temperatureMain.innerText = `${weatherData.currentTemperature}`
+	weatherApp.windspeed.innerText = `${weatherData.currentWindSpeed}Kmph ${decodeWindDirection(weatherData.currentWindDirection)}`
+
+	weatherApp.sunrise.innerText = `Sunrise at ${stringToDate(weatherData.currentSunrise, 'time')}`
+	weatherApp.sunset.innerText = `Sunset at ${stringToDate(weatherData.currentSunset, 'time')}`
+
+	for (let i = 1; i < weatherData.weeklyDates.length - 1; i++) {
+		const weeklyWeatherCode = decodeWeather(weatherData.weeklyWeatherCode[i])
+
+		const weeklyForecastRow = document.createElement('tr')
+		const day = document.createElement('td')
+		const weeklyMinTemperature = document.createElement('td')
+		const weeklyMaxTemperature = document.createElement('td')
+
+		day.innerText = `${stringToDate(weatherData.weeklyDates[i], 'dayName')}`
+		weeklyMinTemperature.innerText = `${weeklyWeatherCode} ${weatherData.weeklyMinTemperature[i]}`
+		weeklyMaxTemperature.textContent = `${weeklyWeatherCode} ${weatherData.weeklyMaxTemperature[i]}`
+
+		weeklyForecastRow.setAttribute(
+			'data-ri-hue',
+			weatherToHue(weatherData.weeklyWeatherCode[i])
+		)
+		weeklyForecastRow.append(day, weeklyMinTemperature, weeklyMaxTemperature)
+		weatherApp.TableForecast.append(weeklyForecastRow)
+		console.log(i, weatherData.weeklyMinTemperature[i])
+	}
 }
 
-window.addEventListener('load', () => {
-	pageInit()
-})
+pageInit()
+// #endregion
